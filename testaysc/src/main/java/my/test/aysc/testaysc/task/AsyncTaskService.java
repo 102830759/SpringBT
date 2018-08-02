2b7f11740671706751ab8b5227aeeb797b718464
huy <huyue@hdsxtech.com>
2018-08-02 14:02:11 +0800

    修改

--- testaysc/src/main/java/my/test/aysc/testaysc/task/AsyncTaskService.java
+++ testaysc/src/main/java/my/test/aysc/testaysc/task/AsyncTaskService.java
@@ -1,9 +1,15 @@
 package my.test.aysc.testaysc.task;
 
+import io.webfolder.cdp.Launcher;
+import io.webfolder.cdp.session.Session;
+import io.webfolder.cdp.session.SessionFactory;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.AsyncResult;
 import org.springframework.stereotype.Service;
 
+import javax.imageio.stream.FileImageOutputStream;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.concurrent.Future;
 
@@ -27,22 +33,59 @@ public class AsyncTaskService {
     /**
      * 异常调用返回Future
      *
-     * @param i
+     * @param
      * @return
      * @throws InterruptedException
      */
     @Async
-    public Future<String> asyncInvokeReturnFuture(int i) throws InterruptedException {
-        System.out.println("input is " + i);
+    public Future<String> asyncInvokeReturnFuture(String url) throws InterruptedException {
+        System.out.println("input is :" + url);
 //        Thread.sleep(1000 * random.nextInt(i));
         Future<String> future;
+        ArrayList<String> command = new ArrayList<String>();
+        //不显示google 浏览器
+        command.add("--headless");
 
-        future = new AsyncResult<String>("线程：" + Thread.currentThread().getName() + "，执行了任务编号：" + i + "任务结果" + doSoming(i));// Future接收返回值，这里是String类型，可以指明其他类型
+        Launcher launcher = new Launcher();
+        try (SessionFactory factory = launcher.launch(command);
+             Session session = factory.create()) {
+            try {
+                session.navigate(url);
+                session.waitDocumentReady(5000);
+
+                // activate the tab/session before capturing the screenshot
+                session.activate();
+                byte[] data = session.captureScreenshot();
+                byte2image(data, "D:\\picture\\" + System.currentTimeMillis() + ".png");
+                System.out.println("线程：" + Thread.currentThread().getName() + "，执行了任务编号：" + url + ",执行成功");
+            } catch (Exception e) {
+                e.printStackTrace();
+                System.out.println("线程：" + Thread.currentThread().getName() + "，执行了任务编号：" + url + ",执行失败");
+            } finally {
+                future = new AsyncResult<String>("线程：" + Thread.currentThread().getName() + "，执行了任务编号：" + url);
+            }
+
+        }
         return future;
 
     }
 
     public int doSoming(int i) {
         return i / i;
+    }
+
+    public static void byte2image(byte[] data, String path) {
+        if (data.length < 3 || path.equals("")) {
+            return;
+        }
+        try {
+            FileImageOutputStream imageOutput = new FileImageOutputStream(new File(path));
+            imageOutput.write(data, 0, data.length);
+            imageOutput.close();
+            System.out.println("Make Picture success,Please find image in " + path);
+        } catch (Exception ex) {
+            System.out.println("Exception: " + ex);
+            ex.printStackTrace();
+        }
     }
 }
